3540298b159ca954d7dd4689de65183b6be79725
StevenRice99 <Contact@StevenRice.ca>
2024-07-24 18:40:56 -0400

    new wave of initial tests

--- llm_tests.py
+++ llm_tests.py
@@ -2,5 +2,5 @@ from main import auto
 from evaluate import evaluate
 
 if __name__ == '__main__':
-    auto("Data/COVID Ontario.csv", 1, 8, 2, 2, 100, 0, 5, 5, {"p": 2, "d": 2, "q": 1}, {"C": 10, "gamma": "auto", "epsilon": 0.01}, True, output="FINAL", model=[None, "Meta-Llama-3.1-405B"], delay=60)
+    auto("Data/COVID Ontario.csv", 1, 8, 2, 2, 100, 0, 5, 5, {"p": 2, "d": 2, "q": 1}, {"C": 10, "gamma": "auto", "epsilon": 0.5}, True, output="FINAL", model=[None, "Meta-Llama-3.1-405B"], delay=60)
     evaluate("Results/FINAL", ["Available", "Needed", "Arrived"], True, 10, 5, False)
